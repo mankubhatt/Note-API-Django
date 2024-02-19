8163dade206594f0244389bada431866638cbb84
mankubhatt <90135816+mankubhatt@users.noreply.github.com>
2024-02-19 15:11:25 +0530

    Update settings.py

--- note_api/settings.py
+++ note_api/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.static.StaticFilesMiddleware',
+    'django.contrib.staticfiles',
 ]
 
 ROOT_URLCONF = 'note_api.urls'
