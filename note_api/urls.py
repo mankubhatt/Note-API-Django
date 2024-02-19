3ed2c3c69c1a373ad5f1f59442911bb04b6cf47a
mankubhatt <90135816+mankubhatt@users.noreply.github.com>
2024-02-19 15:17:54 +0530

    Update urls.py

--- note_api/urls.py
+++ note_api/urls.py
@@ -24,3 +24,6 @@ urlpatterns = [
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
