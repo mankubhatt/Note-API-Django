a30fff018d0ae8b06dcc586f2a0194fa5ae8ddc9
mankubhatt <90135816+mankubhatt@users.noreply.github.com>
2024-02-19 15:20:32 +0530

    Update urls.py

--- note_api/urls.py
+++ note_api/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include, re_path
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
+from note_api import settings
 
 
 schema_view = get_schema_view(
